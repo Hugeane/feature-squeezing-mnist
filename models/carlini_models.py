2437e93f2121ae85e9e9f2b7741acb2fbbd6cb35
Hugeane <Hugeane233@gmail.com>
2024-10-09 22:48:42 +0800

    upgrade library

--- models/carlini_models.py
+++ models/carlini_models.py
@@ -3,17 +3,17 @@ from keras.layers import Dense, Dropout, Activation, Flatten, Lambda
 from keras.layers import MaxPooling2D, Conv2D
 
 
-def carlini_mnist_model(logits=True, input_range_type=2, pre_filter=lambda x:x):
-    input_shape=(28, 28, 1)
+def carlini_mnist_model(logits=True, input_range_type=2, pre_filter=lambda x: x):
+    input_shape = (28, 28, 1)
     nb_filters = 32
-    nb_denses = [200,200,10]
+    nb_denses = [200, 200, 10]
     return carlini_model(input_shape, nb_filters, nb_denses, logits, input_range_type, pre_filter)
 
 
-def carlini_cifar10_model(logits=True, input_range_type=2, pre_filter=lambda x:x):
-    input_shape=(32, 32, 3)
+def carlini_cifar10_model(logits=True, input_range_type=2, pre_filter=lambda x: x):
+    input_shape = (32, 32, 3)
     nb_filters = 64
-    nb_denses = [256,256,10]
+    nb_denses = [256, 256, 10]
     return carlini_model(input_shape, nb_filters, nb_denses, logits, input_range_type, pre_filter)
 
 
@@ -28,14 +28,14 @@ def carlini_model(input_shape, nb_filters, nb_denses, logits, input_range_type, 
     if input_range_type == 1:
         # The input data range is [0, 1]. 
         # Convert to [-0.5,0.5] by x-0.5.
-        scaler = lambda x: x-0.5
+        scaler = lambda x: x - 0.5
     elif input_range_type == 2:
         # The input data range is [-0.5, 0.5].
         # Don't need to do scaling for carlini models, as it is the input range by default.
         scaler = lambda x: x
     elif input_range_type == 3:
         # The input data range is [-1, 1]. Convert to [-0.5,0.5] by x/2.
-        scaler = lambda x: x/2
+        scaler = lambda x: x / 2
 
     model.add(Lambda(scaler, input_shape=input_shape))
     model.add(Lambda(pre_filter, output_shape=input_shape))
@@ -46,9 +46,9 @@ def carlini_model(input_shape, nb_filters, nb_denses, logits, input_range_type, 
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(nb_filters*2, (3, 3)))
+    model.add(Conv2D(nb_filters * 2, (3, 3)))
     model.add(Activation('relu'))
-    model.add(Conv2D(nb_filters*2, (3, 3)))
+    model.add(Conv2D(nb_filters * 2, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
--- version.md
+++ version.md
@@ -1,7 +1,10 @@
 libraries version
-
+由于cleverhans3.1.0支持python3.6 tf1或2 更高版本不能同时支持python3.6和tf1 需要降到tf1.12.0
 ```tex
 python 3.6.0
 tensorflow 1.12.0
+keras 2.3.1
+cleverhans 3.1.0
+urllib3
 ```
 
